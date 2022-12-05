7476df2777e03e0317941f6707932bd0f0a233f4
soufyanbit <116471096+soufyanbit@users.noreply.github.com>
2022-12-05 01:46:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -67,8 +67,8 @@ global.ppulsa2 = '08XXXXXXXXXX' // kalau ada kartu lain isi aja
 global.psaweria = 'https://saweria.com/mrw4h1d'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '62XXXXXXXXXXX'
-global.nomorown = '6282127487538'
+global.nomorbot = '212656724018'
+global.nomorown = '212619508774'
 global.namebot = 'WH-MODS-BOT-V1'
 global.nameown = 'WH MODS DEV'
 
